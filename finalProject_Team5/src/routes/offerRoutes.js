99da2de3d2aa83722c433333f5dacfbea0a0d171
Aya Ahmed <ayaahmed.com2006@gmail.com>
2026-09-18 21:07:00 +0300

    protect offer routes with admin middleware

--- finalProject_Team5/src/routes/offerRoutes.js
+++ finalProject_Team5/src/routes/offerRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { createOffer, getOffers } = require('../controllers/offerController');
 
+const authMiddleware = require('../middlewares/auth.middleware'); 
+
 router.get('/', getOffers);
 
-router.post('/', createOffer);
+router.post('/', authMiddleware, createOffer); 
+
 
 module.exports = router;
