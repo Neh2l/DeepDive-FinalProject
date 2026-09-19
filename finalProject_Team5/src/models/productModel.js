2d222599a28e8cf2d4bb3977ce145fb43a5e554a
Neh2l <nehalreda14@gmail.com>
2026-09-19 15:31:24 +0300

    Add product rate

--- finalProject_Team5/src/models/productModel.js
+++ finalProject_Team5/src/models/productModel.js
@@ -1,24 +1,35 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+
   title: { type: String, required: true },
+
   description: { type: String, required: true },
+
   price: { type: Number, required: true, min: 0 },
+
   stock: { type: Number, required: true, min: 0 },
+
+  rate: { type: Number, min: 0, max: 5, default: 0 },
+
   category: { type: String, required: true },
+
   images: [
     {
       url: {
         type: String,
         required: true
       },
+
       public_id: {
         type: String,
         required: true
       }
     }
   ]
+
 }, 
+
 { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
